34df51d4adf95dd138935ad421af39f5e825af0f
Ian Bailey <ianbailey@pursuits-macbook-pro-77.local>
2018-11-30 11:43:44 -0500

    got colors to display

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CrayonDetailViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Ian Bailey on 11/30/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class CrayonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Alex Paul on 11/15/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class CrayonViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    private var crayons = [Crayon]()
+    
+    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    crayons = Crayon.allTheCrayons
+    tableView.dataSource = self
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
+extension CrayonViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayons.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CrayonCell", for: indexPath)
+        let crayon = crayons[indexPath.row]
+        cell.textLabel?.text = crayon.name
+        cell.detailTextLabel?.text = crayon.hex
+        cell.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red)/255, green: CGFloat(crayon.green)/255, blue: CGFloat(crayon.blue)/255, alpha:1.0)
+        return cell
+    }
+}
